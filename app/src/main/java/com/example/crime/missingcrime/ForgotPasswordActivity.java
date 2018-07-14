895bded9507f7ebcaf54027739e3770a5bb06ff2
Zubaer <zubaer.haque@gmail.com>
2018-07-14 13:04:31 +0600

    Map activity lunched and menu added

--- app/src/main/java/com/example/crime/missingcrime/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/crime/missingcrime/ForgotPasswordActivity.java
@@ -1,13 +1,67 @@
 package com.example.crime.missingcrime;
 
+import android.app.ProgressDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
+    private EditText email;
+    private Button reset, signIn;
+    private ProgressDialog pBar;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+    private FirebaseDatabase database;
+    private FirebaseAuth.AuthStateListener mAuthListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+        mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        pBar=new ProgressDialog(this);
+        button();
+        edittext();
     }
+    void button()
+    {
+        reset=findViewById(R.id.buttonSend);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendresetemail();
+            }
+        });
+
+    }
+    void edittext()
+    {
+
+    }
+    void sendresetemail()
+    {
+        String sEmail=email.getText().toString().trim();
+        mAuth.sendPasswordResetEmail(sEmail)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(ForgotPasswordActivity.this, "Email Sent, check it", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/example/crime/missingcrime/MainActivity.java
+++ app/src/main/java/com/example/crime/missingcrime/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.crime.missingcrime;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
     private FirebaseAuth mAuth;
     private FirebaseUser user;
     @Override
@@ -16,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
         user = FirebaseAuth.getInstance().getCurrentUser();
+        button();
+
     }
 
     @Override
@@ -27,5 +41,58 @@ public class MainActivity extends AppCompatActivity {
             Intent intentLogin= new Intent(this, SignInActivity.class );
             startActivity(intentLogin);
         }
+    }
+    void button(){
+
+    }
+    public boolean isServicesOK(){
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if(available == ConnectionResult.SUCCESS){
+            //everything is fine and the user can make map requests
+            Toast.makeText(this, "isServicesOK: Google Play Services is working", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+        else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            //an error occured but we can resolve it
+            Toast.makeText(this, "isServicesOK: an error occured but we can fix it", Toast.LENGTH_SHORT).show();
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_home:
+                Intent intentMapView = new Intent(this, MainActivity.class);
+                startActivity(intentMapView);
+                break;
+
+            case R.id.menu_logout:
+                if(isServicesOK()){
+                    signOut();
+                }
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    void signOut()
+    {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+        startActivity(intent);
+        finish();
+        return;
     }
 }
